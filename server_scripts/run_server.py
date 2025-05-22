20fb930731056ba7dc45f691e8bacd7a9391e4a2
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2025-05-22 17:44:52 +0200

    del api cause useless

--- server_scripts/run_server.py
+++ server_scripts/run_server.py
@@ -1,7 +1,5 @@
 from flask import redirect
-from app import app, fetch_and_store_word
+from app import app
 
 if __name__ == '__main__':
-    fetch_and_store_word('conquest')
-    fetch_and_store_word('despair')
     app.run(host='0.0.0.0', port=5000)
